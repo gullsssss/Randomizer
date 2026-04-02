5db62bbc20033e577e06d9119bd35303ff82d808
gullsssss <kapustaa356@gmail.com>
2026-04-02 16:32:51 +0300

    Update randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -1,12 +1,12 @@
 import random
 print("Это генератор ранжомных чисел, чтобы использовать")
 while True:
-    a = int(input("Введите от скольки будет идти счет: "))
-    b = int(input("Введите до скольки будет идти счет: "))
+    Fn = int(input("Введите от скольки будет идти счет: "))
+    Sn = int(input("Введите до скольки будет идти счет: "))
     if a > b:
         print("Число от скольки не может быть больше до скольки, введите все заново")
         continue
-    c = random.randint(a,b)
+    result = random.randint(Fn,Sn)
     print("Результат: ",c)
     con = input("Продолжить? y/n :")
     if con != "y":
